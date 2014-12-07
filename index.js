b7a3abf084c05d8aa4ad9aef83084f39b9e7d6fd
Andrew Hathaway <andrew@andrewhathaway.net>
2014-12-07 23:09:21 +0000

    Setup redis, memcached wrappers

---
+++ index.js
@@ -0,0 +1,19 @@
+var RedisCache     = require('./lib/redisCache');
+var MemcachedCache = require('./lib/memcachedCache');
+
+/**
+ * Cache All The Things
+ * @todo: Custom cache handlers
+ */
+module.exports = function(service, config) {
+
+  if (service == 'redis') {
+    return new RedisCache(config);
+  }
+
+  if (service == 'memcached') {
+    return new MemcachedCache(config);
+  }
+
+  return {};
+};
