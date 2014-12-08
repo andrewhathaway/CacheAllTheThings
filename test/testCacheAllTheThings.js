ce52b37873c55045b97c4288e9d6b8dfdd3a03f8
Andrew Hathaway <andrew@andrewhathaway.net>
2014-12-08 21:42:44 +0000

    Add more tests to set and get values from Redis

--- test/testCacheAllTheThings.js
+++ test/testCacheAllTheThings.js
@@ -8,4 +8,47 @@ describe('CacheAllTheThings', function() {
     Assert(inst.name, 'RedisCache');
   });
 
+  describe('Redis', function() {
+
+    it('Should set a key', function(done) {
+      var redisInst = new CacheAllTheThings('redis');
+
+      redisInst
+        .set('lol', 'hai')
+        .then(function(e) {
+          Assert.equal(e, null);
+          done();
+        });
+    });
+
+    it('Should get a key that exists', function(done) {
+      var redisInst = new CacheAllTheThings('redis');
+
+      redisInst
+        .get('lol')
+        .then(function(val) {
+          Assert(val, 'hai');
+          done();
+        }, function(e) {
+          Assert.notEqual(e, null);
+          done();
+        });
+    });
+
+    it('Should get a key that does not exist', function(done) {
+      var redisInst = new CacheAllTheThings('redis');
+
+      redisInst
+        .get('lols')
+        .then(function(val) {
+          Assert(true);
+          done();
+        }, function(e) {
+          Assert.notEqual(e, null);
+          done();
+        });
+    });
+
+  });
+
 });
