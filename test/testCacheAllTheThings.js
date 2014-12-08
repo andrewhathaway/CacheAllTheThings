0d6dc4c64e51d7fce48b970296d9ac4dcab0857b
Andrew Hathaway <andrew@andrewhathaway.net>
2014-12-08 22:22:30 +0000

    Memcached tests

--- test/testCacheAllTheThings.js
+++ test/testCacheAllTheThings.js
@@ -97,4 +97,90 @@ describe('CacheAllTheThings', function() {
 
   });
 
+  /*
+   * Memcached Tests
+   */
+  describe('Memcached', function() {
+
+    it('Should set a key', function(done) {
+      var redisInst = new CacheAllTheThings('memcached');
+
+      redisInst
+        .set('lol', 'hai')
+        .then(function(e) {
+          Assert.equal(e, null);
+          done();
+        });
+    });
+
+    it('Should get a key that exists', function(done) {
+      var redisInst = new CacheAllTheThings('memcached');
+
+      redisInst
+        .get('lol')
+        .then(function(val) {
+          Assert(val, 'hai');
+          done();
+        }, function(e) {
+          Assert.notEqual(e, null);
+          done();
+        });
+    });
+
+    it('Should get a key that does not exist', function(done) {
+      var redisInst = new CacheAllTheThings('memcached');
+
+      redisInst
+        .get('lols')
+        .then(function(val) {
+          Assert(true);
+          done();
+        }, function(e) {
+          Assert.notEqual(e, null);
+          done();
+        });
+    });
+
+    it('Should set an object, and get back an object', function(done) {
+      var redisInst = new CacheAllTheThings('memcached');
+
+      redisInst
+        .set('obj', {
+          testing : true
+        })
+        .then(function() {
+
+          redisInst
+            .get('obj')
+            .then(function(val) {
+              Assert(val, {
+                testing : true
+              });
+              done();
+            }, function(e) {
+              done();
+            });
+
+        });
+    });
+
+    it('Should be able to delete a key', function(done) {
+      var redisInst = new CacheAllTheThings('memcached');
+
+      redisInst
+        .del('lol')
+        .then(function() {
+
+          redisInst
+            .get('lol')
+            .then(function(val) {
+              Assert.equal(val, null);
+              done();
+            });
+
+        });
+    });
+
+  });
+
 });
