2906b30ce73c4027d09dea34a493bc793645a0b2
Andrew Hathaway <andrew@andrewhathaway.net>
2014-12-08 20:21:06 +0000

    Initial test to see that asking for a redis instance, returns one

--- test/testCacheAllTheThings.js
+++ test/testCacheAllTheThings.js
@@ -1,14 +1,11 @@
-var Assert = require('assert');
+var Assert            = require('assert');
+var CacheAllTheThings = require('../');
 
 describe('CacheAllTheThings', function() {
 
-  /*
-   * Test Connections
-   */
-  describe('Connect', function() {
-    it('Should connect', function() {
-
-    });
+  it('When asked to boot a Redis instance, it should do so', function() {
+    var inst = new CacheAllTheThings('redis');
+    Assert(inst.name, 'RedisCache');
   });
 
 });
